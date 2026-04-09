8900c6f49bd12c74f4cc0daaf222f527a7632470
Joaquín Díaz <mjoaquind@gmail.com>
2026-04-09 12:30:25 -0300

    primeros cambios

---
+++ js/countdown-custom.js
@@ -0,0 +1,7 @@
+(function($) {	
+	'use strict';
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2025, 10-1, 30, 18)});  //  year, month, date, hour
+        });
+})(jQuery);
+
