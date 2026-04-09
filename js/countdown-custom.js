719988bdd09575339cb02e3b7b1df5c3ee0c0cdb
Joaquín Díaz <mjoaquind@gmail.com>
2026-04-09 17:55:41 -0300

    arreglo fecha countdown

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -2,7 +2,7 @@
   "use strict";
   jQuery(function () {
     jQuery("#defaultCountdown").countdown({
-      until: new Date(2026, 10 - 1, 30, 18),
+      until: new Date(2026, 10 - 1, 29, 18),
     }); //  year, month, date, hour
   });
 })(jQuery);
